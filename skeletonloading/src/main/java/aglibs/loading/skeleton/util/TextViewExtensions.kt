856c362962b4cc874e2470164e8845aec7595187
Agnaldo do Nascimento Pereira <agnaldo.pereira@etec.sp.gov.br>
2020-04-23 22:18:08 -0300

    Adjusts on multi line text and configs

--- skeletonloading/src/main/java/aglibs/loading/skeleton/util/TextViewExtensions.kt
+++ skeletonloading/src/main/java/aglibs/loading/skeleton/util/TextViewExtensions.kt
@@ -11,73 +11,100 @@ fun TextView.getAllLineBounds(
 ): ArrayList<Rect> {
     val allLineBounds = ArrayList<Rect>()
     val lineCount = this.lineCount
-    val lineSpacingExtra = this.lineSpacingExtra
-    val lineSpacingMultiplier = this.lineSpacingMultiplier
+    if (splitSkeletonTextByLines) {
+        for (line in 0 until lineCount) {
+            allLineBounds.add(
+                getSkeletonLineBounds(
+                    this, line,
+                    splitSkeletonTextByLines,
+                    reduceToTextSpace
+                )
+            )
+        }
+    } else {
+        allLineBounds.add(
+            getSkeletonLineBounds(
+                this, 0,
+                splitSkeletonTextByLines,
+                reduceToTextSpace
+            )
+        )
+    }
+    return allLineBounds
+}
+
+private fun getSkeletonLineBounds(
+    textView: TextView,
+    line: Int,
+    splitSkeletonTextByLines: Boolean,
+    reduceToTextSpace: Boolean
+): Rect {
+    val lineCount = textView.lineCount
+    val lineSpacingExtra = textView.lineSpacingExtra
+    val lineSpacingMultiplier = textView.lineSpacingMultiplier
     val lineSpace = lineSpacingExtra * lineSpacingMultiplier
-    if (splitSkeletonTextByLines && lineSpace > 0) {
-        for (i in 0 until lineCount) {
-            val lineBounds = Rect()
-            this.getLineBounds(i, lineBounds)
-            if (i != lineCount - 1) {
-                lineBounds.bottom -= lineSpace.toInt()
+
+    val lineBounds = Rect()
+    textView.getLineBounds(line, lineBounds)
+
+    if (splitSkeletonTextByLines) {
+        if (line != lineCount - 1) {
+            lineBounds.bottom -= lineSpace.toInt()
+        }
+    }
+
+    if (reduceToTextSpace) {
+        val boundWidth = lineBounds.width()
+        val lineWidth = textView.layout.getLineWidth(line)
+        val whiteSpace = boundWidth - lineWidth
+        val isRtl = textView.layoutDirection == View.LAYOUT_DIRECTION_RTL
+
+        when (textView.textAlignment) {
+            View.TEXT_ALIGNMENT_CENTER -> {
+                lineBounds.left += (whiteSpace / 2).toInt()
+                lineBounds.right -= (whiteSpace / 2).toInt()
             }
-
-            if (reduceToTextSpace) {
-                val boundWidth = lineBounds.width()
-                val lineWidth = this.layout.getLineWidth(i)
-                val whiteSpace = boundWidth - lineWidth
-                val isRtl = this.layoutDirection == View.LAYOUT_DIRECTION_RTL
-
-                when (this.textAlignment) {
-                    View.TEXT_ALIGNMENT_CENTER -> {
+            View.TEXT_ALIGNMENT_VIEW_START, View.TEXT_ALIGNMENT_TEXT_START -> {
+                if (isRtl) {
+                    lineBounds.left += whiteSpace.toInt()
+                } else {
+                    lineBounds.right -= whiteSpace.toInt()
+                }
+            }
+            View.TEXT_ALIGNMENT_VIEW_END, View.TEXT_ALIGNMENT_TEXT_END -> {
+                if (isRtl) {
+                    lineBounds.right -= whiteSpace.toInt()
+                } else {
+                    lineBounds.left += whiteSpace.toInt()
+                }
+            }
+            View.TEXT_ALIGNMENT_GRAVITY -> {
+                when (textView.layout?.alignment) {
+                    Layout.Alignment.ALIGN_NORMAL -> {
+                        if (isRtl) {
+                            lineBounds.left += whiteSpace.toInt()
+                        } else {
+                            lineBounds.right -= whiteSpace.toInt()
+                        }
+                    }
+                    Layout.Alignment.ALIGN_CENTER -> {
                         lineBounds.left += (whiteSpace / 2).toInt()
                         lineBounds.right -= (whiteSpace / 2).toInt()
                     }
-                    View.TEXT_ALIGNMENT_VIEW_START, View.TEXT_ALIGNMENT_TEXT_START -> {
-                        if (isRtl) {
-                            lineBounds.left += whiteSpace.toInt()
-                        } else {
-                            lineBounds.right -= whiteSpace.toInt()
-                        }
-                    }
-                    View.TEXT_ALIGNMENT_VIEW_END, View.TEXT_ALIGNMENT_TEXT_END -> {
+                    Layout.Alignment.ALIGN_OPPOSITE -> {
                         if (isRtl) {
                             lineBounds.right -= whiteSpace.toInt()
                         } else {
                             lineBounds.left += whiteSpace.toInt()
                         }
-                    }
-                    View.TEXT_ALIGNMENT_GRAVITY -> {
-                        when (this.layout.alignment) {
-                            Layout.Alignment.ALIGN_NORMAL -> {
-                                if (isRtl) {
-                                    lineBounds.left += whiteSpace.toInt()
-                                } else {
-                                    lineBounds.right -= whiteSpace.toInt()
-                                }
-                            }
-                            Layout.Alignment.ALIGN_CENTER -> {
-                                lineBounds.left += (whiteSpace / 2).toInt()
-                                lineBounds.right -= (whiteSpace / 2).toInt()
-                            }
-                            Layout.Alignment.ALIGN_OPPOSITE -> {
-                                if (isRtl) {
-                                    lineBounds.right -= whiteSpace.toInt()
-                                } else {
-                                    lineBounds.left += whiteSpace.toInt()
-                                }
-                            }
-                        }
-                    }
-                    else -> {
-                        lineBounds.right -= whiteSpace.toInt()
                     }
                 }
             }
-            allLineBounds.add(lineBounds)
+            else -> {
+                lineBounds.right -= whiteSpace.toInt()
+            }
         }
-    } else {
-        allLineBounds.add(Rect(0, 0, this.width, this.height))
     }
-    return allLineBounds
+
+    return lineBounds
 }
