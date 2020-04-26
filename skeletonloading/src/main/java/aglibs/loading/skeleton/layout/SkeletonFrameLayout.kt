6604f87197b3a7f7cf9452a80a2b34c648600100
Agnaldo do Nascimento Pereira <agnaldo.pereira@etec.sp.gov.br>
2020-04-25 22:48:35 -0300

    Add SkeletonFramelayout and SkeletonGridLayout

---
+++ skeletonloading/src/main/java/aglibs/loading/skeleton/layout/SkeletonFrameLayout.kt
@@ -0,0 +1,48 @@
+package aglibs.loading.skeleton.layout
+
+import aglibs.loading.skeleton.drawer.ISkeletonDrawer
+import aglibs.loading.skeleton.drawer.SkeletonViewGroupDrawer
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class SkeletonFrameLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr),
+    ISkeletonDrawer {
+
+    private var skeletonDrawer = SkeletonViewGroupDrawer(this).apply {
+        getStyles(attrs, defStyleAttr)
+    }
+
+    init {
+        setWillNotDraw(false)
+    }
+
+    override fun getSkeletonDrawer() = skeletonDrawer
+
+    override fun isLoading() = skeletonDrawer.isLoading()
+
+    override fun startLoading() {
+        skeletonDrawer.startLoading()
+    }
+
+    override fun stopLoading() {
+        skeletonDrawer.stopLoading()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        if (!skeletonDrawer.draw(canvas)) {
+            super.onDraw(canvas)
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        stopLoading()
+        super.onDetachedFromWindow()
+    }
+
+}
